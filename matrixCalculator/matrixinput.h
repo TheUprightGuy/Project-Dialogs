9d29258db33be6231519d7d664ebd4ac3b42b794
Batman <Corey.Fre7626@1815C014>
2017-10-02 10:29:54 +1300

    New class file.

---
+++ matrixCalculator/matrixinput.h
@@ -0,0 +1,19 @@
+#ifndef __MATRIXINPUT_H__
+#define __MATRIXINPUT_H__
+
+class CMatrix
+{
+public:
+	void GetMatrixA()
+	{
+	
+	};
+
+	void GetMatrixB()
+	{};
+
+private:
+
+};
+
+#endif // __MATRIXINPUT_H__

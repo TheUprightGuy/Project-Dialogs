c6b391e7332173ccfa81ddcdfdc3d24aaf5ea2d7
Cake <CakeFactory@outlook.co.nz>
2017-10-05 22:16:42 +1300

    Pull.

--- matrixCalculator/inverse.cpp
+++ matrixCalculator/inverse.cpp
@@ -6,7 +6,24 @@ if(_ADeterminant = 0)
 }
 else
 {
+    matrix inverse(matrix M){
+        matrix I = identity(M.size());
+        for(int K=0; K<rows; ++K){
+            //one in the pivot
+            double factor = M[K][K];
+            M[K] /= factor;
+            I[K] /= factor;
+            //zeroing the column
+            for(int L=0; L<rows; ++L){
+                if( K==L ) continue;
+                double coefficient = M[L][K];
+                M[L] -= coefficient*M[K];
+                I[L] -= coefficient*I[K];
+            }
+        }
 
+    return I;
+    }
 }
 
 */

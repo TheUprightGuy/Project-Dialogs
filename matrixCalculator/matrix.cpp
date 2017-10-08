2bb415813cf1ab5cac89d6df3591b3698a7c67fe
Batman <Corey.Fre7626@1815C014>
2017-10-09 12:07:11 +1300

    Commit

--- matrixCalculator/matrix.cpp
+++ matrixCalculator/matrix.cpp
@@ -17,6 +17,11 @@ CMatrix::CMatrix(HWND _hwnd)
 	}
 }
 
+CMatrix::~CMatrix()
+{
+
+};
+
 void CMatrix::GetMatrixA()
 {
 	iMatrixA[0][0] = static_cast<int>(utils::ReadFromEditBox(hwnd, IDC_EDIT_A11));
@@ -303,11 +308,109 @@ void CMatrix::MatrixInverse(bool bAorB)
 {
 	if (bAorB)//If getting inverse of A
 	{
-		GetMatrixA();
+		{
+			int i, j, k, n;
+			float a[10][10] = { 0 }, d;
+			n = 4;
+			for (i = 1; i <= n; i++)
+				for (j = 1; j <= n; j++)
+					a[i][j] = iMatrixA[i][j];
+
+			for (i = 1; i <= n; i++)
+				for (j = 1; j <= 2 * n; j++)
+					if (j == (i + n))
+						a[i][j] = 1;
+
+			/************** partial pivoting **************/
+			for (i = n; i > 1; i--)
+			{
+				if (a[i - 1][1] < a[i][1])
+					for (j = 1; j <= n * 2; j++)
+					{
+						d = a[i][j];
+						a[i][j] = a[i - 1][j];
+						a[i - 1][j] = d;
+					}
+			}
+			/********** reducing to diagonal  matrix ***********/
+
+			for (i = 1; i <= n; i++)
+			{
+				for (j = 1; j <= n * 2; j++)
+					if (j != i)
+					{
+						d = a[j][i] / a[i][i];
+						for (k = 1; k <= n * 2; k++)
+							a[j][k] -= a[i][k] * d;
+					}
+			}
+			/************** reducing to unit matrix *************/
+			for (i = 1; i <= n; i++)
+			{
+				d = a[i][i];
+				for (j = 1; j <= n * 2; j++)
+					a[i][j] = a[i][j] / d;
+			}
+
+			for (i = 1; i <= n; i++)
+			{
+				for (j = n + 1; j <= n * 2; j++)
+					a[i][j] = iMatrixR[i][j];
+			}
+		}
 	}
 	else //If getting inverse of b
 	{
-		GetMatrixB();
+		{
+			int i, j, k, n;
+			float a[10][10] = { 0 }, d;
+			n = 4;
+			for (i = 1; i <= n; i++)
+				for (j = 1; j <= n; j++)
+					a[i][j] = iMatrixB[i][j];
+
+			for (i = 1; i <= n; i++)
+				for (j = 1; j <= 2 * n; j++)
+					if (j == (i + n))
+						a[i][j] = 1;
+
+			/************** partial pivoting **************/
+			for (i = n; i > 1; i--)
+			{
+				if (a[i - 1][1] < a[i][1])
+					for (j = 1; j <= n * 2; j++)
+					{
+						d = a[i][j];
+						a[i][j] = a[i - 1][j];
+						a[i - 1][j] = d;
+					}
+			}
+			/********** reducing to diagonal  matrix ***********/
+
+			for (i = 1; i <= n; i++)
+			{
+				for (j = 1; j <= n * 2; j++)
+					if (j != i)
+					{
+						d = a[j][i] / a[i][i];
+						for (k = 1; k <= n * 2; k++)
+							a[j][k] -= a[i][k] * d;
+					}
+			}
+			/************** reducing to unit matrix *************/
+			for (i = 1; i <= n; i++)
+			{
+				d = a[i][i];
+				for (j = 1; j <= n * 2; j++)
+					a[i][j] = a[i][j] / d;
+			}
+
+			for (i = 1; i <= n; i++)
+			{
+				for (j = n + 1; j <= n * 2; j++)
+					a[i][j] = iMatrixR[i][j];
+			}
+		}
 	}
 	WriteToMatrixR();
 }

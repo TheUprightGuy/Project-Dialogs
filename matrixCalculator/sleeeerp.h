698ef23ed15de307cfaad0e6fd8b1e2bb38db123
Batman <Corey.Fre7626@1815C014>
2017-10-30 09:02:21 +1300

    sleeeeerp

---
+++ matrixCalculator/sleeeerp.h
@@ -0,0 +1,19 @@
+#ifndef __SLERP_H__
+#define __SLERP_H__
+
+class CSlerp
+{
+public:
+	CSlerp();
+	~CSlerp();
+
+	void SlerpABT();
+	void ConvertA();
+	void ConvertB();
+	void ConvertSlerp();
+
+private:
+
+};
+
+#endif //__SLERP_H__
